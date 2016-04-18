7305b092d2f2ca545790e4c4658a2e253d419026
Parker Illig <pail4944@colorado.edu>
2016-04-18 14:59:40 -0600

    added new function and corresponding test case

--- geometry.c
+++ geometry.c
@@ -19,6 +19,10 @@
 
 #define DEBUG(file, line, func, msg) fprintf(stderr, "DEBUG - %s_%d_%s: %s", file, line, func, msg);
 
+double coord_2d_area_triangle(const coord_2d_t *a, const coord_2d_t *b, const coord_2d_t *c){
+    return abs((a->x - b->x)*(0.5*(a->y - c->y)));
+}
+
 double coord_2d_dist(const coord_2d_t* a, const coord_2d_t* b){
 
     /* Input Checks */
